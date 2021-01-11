ad9af36cb7c5bfdfcdc350064818b49423760448
Sebastian J <kenttzz123@gmail.com>
2021-01-11 18:09:19 +0100

    first commit

---
+++ src/main/java/pl/edu/wszib/lab1/examplepackage/ExampleClass.java
@@ -0,0 +1,4 @@
+package pl.edu.wszib.lab1.examplepackage;
+
+public class ExampleClass {
+}
---
+++ src/main/java/pl/edu/wszib/lab1/main.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.lab1;
+
+import java.util.stream.Stream;
+
+public class main {
+    public static void main(String[] args) {
+
+
+    }
+}
